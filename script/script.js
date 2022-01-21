b7598adcd06d83410443c226b7f692730fdebdd7
NikolayNikolay <96984183+NikolayNikolay@users.noreply.github.com>
2022-01-21 08:01:05 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,30 @@
+
+//-------слайдер---------------------------------------------//
+$(document).ready(function () {
+   $('.block__title').click(function (event) {
+      if ($('.block').hasClass('one')) {
+         $('.block__title').not($(this)).removeClass('active');
+         $('.block__text').not($(this).next()).slideUp(300);
+      }
+      $(this).toggleClass('active').next().slideToggle(300);
+   });
+});
+//----------------------------------------------------------//
+//--------------бургер------------------------------//
+$(document).ready(function () {
+   $('.header__burger').click(function (event) {
+      $('.header__burger,.header__Menu').toggleClass('active1')
+      $('.header__Menu').toggleClass('active2')
+   });
+});
+
+
+$(document).ready(function () {
+   $('.block__title_defends').click(function (event) {
+      if ($('.block').hasClass('one')) {
+         $('.block__title_defends').not($(this)).removeClass('active');
+         //$('.block__text_defends').not($(this).next()).slideUp(300);
+      }
+      $(this).toggleClass('active').next().slideToggle(300);
+   });
+});
